dd9b5cd9dd0ddb3280d4b5ce6453449ac0f6799e
Marcos Tomaz <marcosvto1@gmail.co>
2020-11-24 20:30:32 -0300

    refactor: add control signal process api

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,35 @@
+import { Logger } from '@overnightjs/logger';
 import {SetupServer} from './server';
 
-function bootstrap() {
-  const appServer = new SetupServer();
-  appServer.start(3000);
+enum ExitStatus {
+  Failure = 1,
+  Success = 0,
+}
+
+async function bootstrap() {
+  try {
+    const appServer = new SetupServer();
+    appServer.start(3000);
+  
+    // Signal
+    const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT'];
+    exitSignals.map((signal) => {
+      process.on(signal, async () => {
+        try {
+          await appServer.close();
+          Logger.Info('App exited with success');
+          process.exit(ExitStatus.Success)
+        } catch (error) {
+          Logger.Err('App exited with error' + error);
+          process.exit(ExitStatus.Failure)
+        }
+      })
+    })
+  } catch (error) {
+    Logger.Err('App exited with error' + error);  
+    process.exit(ExitStatus.Failure);
+  }
+ 
 }
 
 bootstrap();
